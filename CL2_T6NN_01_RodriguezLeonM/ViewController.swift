091bf2e1a3d9d5352abf7a9f424f4580c61060ba
Mitchell Rodríguez <mitrodle@gmail.com>
2021-05-30 11:16:26 -0500

    Initial Commit

---
+++ CL2_T6NN_01_RodriguezLeonM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CL2_T6NN_01_RodriguezLeonM
+//
+//  Created by Mitchell on 30/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
